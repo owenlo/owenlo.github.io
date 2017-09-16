8bd8ad662dfb1aa145fe8c36f50dc785a0a4c464
owenlo <owenlo@gmail.com>
2017-09-16 01:16:00 +0100

    Average watts added

--- scripts/site.js
+++ scripts/site.js
@@ -9,9 +9,11 @@ $(document).ready(function() {
     var max_elevation = [];
     var calories = [];
     var cum_calories = [];
-    var time = [];
+    var cum_time = [];
     var cum_elevation = [];
     var cum_speed = [];
+	var avg_watts = [];
+	var cum_avg_watts = [];
 
     dialog = $("#dialog-form").dialog({
         closeOnEscape: false,
@@ -33,10 +35,10 @@ $(document).ready(function() {
                     success: function(json) {
                         parseStravaData(json);
                         mapData(json);
-                        plotGraphType1('Distance Travelled', distance, cum_distance, 'Distance', 'Miles', 'plotDistance');
+                        plotGraphType1('Distance Travelled', 'Cumulative', distance, cum_distance, 'Distance', 'Miles', 'Miles', 'plotDistance');
                         plotGraphType2('Elevation Gain', 'Min Elevation', 'Max Elevation', elevation, min_elevation, max_elevation, 'Elevation', 'Meters', 'plotElevation');
                         plotGraphType3('Max Speed', 'Avg Speed', max_speed, avg_speed, 'Speed', 'Miles Per Hour', 'plotSpeed');
-                        plotGraphType1('Calories', calories, cum_calories, 'Calories', 'Kilojoules', 'plotCalories');
+                        plotGraphType1('Calories', 'Avg Watts', calories, avg_watts, 'Energy', 'Kilojoules', 'Watts', 'plotPower');
                         generateStatistics();
                         $("#plots").show();
                     }
@@ -65,19 +67,22 @@ $(document).ready(function() {
             min_elevation.push(json[i].elev_low);
             max_elevation.push(json[i].elev_high);
             calories.push(round((json[i].kilojoules), 2));
+			avg_watts.push(json[i].average_watts);
 
             if (i == 0) {
                 cum_distance.push(round((json[i].distance * 0.000621371), 2));
                 cum_calories.push(round((json[i].kilojoules), 2));
-                time.push(json[i].elapsed_time / 60);
+                cum_time.push(json[i].elapsed_time / 60);
                 cum_elevation.push(json[i].total_elevation_gain);
                 cum_speed.push(round((json[i].average_speed * 2.23694), 2));
+				cum_avg_watts.push(json[i].average_watts);
             } else {
                 cum_distance.push(round((cum_distance[i - 1] + json[i].distance * 0.000621371), 2));
                 cum_calories.push(round((cum_calories[i - 1] + json[i].kilojoules), 2));
-                time.push(round((time[i - 1] + json[i].elapsed_time / 60), 2));
+                cum_time.push(round((cum_time[i - 1] + json[i].elapsed_time / 60), 2));
                 cum_elevation.push(cum_elevation[i - 1] + json[i].total_elevation_gain);
                 cum_speed.push(round((cum_speed[i - 1] + json[i].average_speed * 2.23694), 2));
+				cum_avg_watts.push(cum_avg_watts[i - 1] + json[i].average_watts);
             }
         }
 
@@ -89,16 +94,17 @@ $(document).ready(function() {
         min_elevation.reverse();
         max_elevation.reverse();
         calories.reverse();
+		avg_watts.reverse();
     }
 
-    function plotGraphType1(label, data1, data2, graphText, yAxisLabel, elementId) {
+    function plotGraphType1(label1, label2, data1, data2, graphText, yAxisLabel1, yAxisLabel2, elementId) {
 
         var options = {
             type: 'bar',
             data: {
                 labels: x_range,
                 datasets: [{
-                    label: label,
+                    label: label1,
                     data: data1,
                     borderColor: "#FF3333",
                     yAxisID: "y-axis-0",
@@ -106,7 +112,7 @@ $(document).ready(function() {
                     fill: false
                 }, {
                     type: 'line',
-                    label: "Cumulative",
+                    label: label2,
                     yAxisID: "y-axis-1",
                     "data": data2,
                     fill: false
@@ -142,7 +148,7 @@ $(document).ready(function() {
                             display: true,
                             scaleLabel: {
                                 display: true,
-                                labelString: yAxisLabel
+                                labelString: yAxisLabel1
                             },
                             position: "left",
                             id: "y-axis-0"
@@ -151,7 +157,7 @@ $(document).ready(function() {
                             display: true,
                             scaleLabel: {
                                 display: true,
-                                labelString: yAxisLabel
+                                labelString: yAxisLabel2
                             },
                             position: "right",
                             id: "y-axis-1"
@@ -348,17 +354,18 @@ $(document).ready(function() {
         // zoom the map to the polyline
         map.fitBounds(polyline.getBounds());
     }
-
+	
     function generateStatistics() {
         $("#count").html(x_range.length);
         $("#total_dist").html(round((cum_distance[cum_distance.length - 1]), 2));
         $("#avg_dist").html(round((cum_distance[cum_distance.length - 1] / cum_distance.length), 2));
         $("#total_elev").html(round(cum_elevation[cum_elevation.length - 1], 2));
         $("#avg_elevation").html(round((cum_elevation[cum_elevation.length - 1] / cum_elevation.length), 2));
-        $("#total_time").html(round((time[time.length - 1] / 60), 2));
-        $("#avg_time").html(round((time[time.length - 1] / time.length), 2));
+        $("#total_time").html(round((cum_time[cum_time.length - 1] / 60), 2));
+        $("#avg_time").html(round((cum_time[cum_time.length - 1] / cum_time.length), 2));
         $("#avg_speed").html(round((cum_speed[cum_speed.length - 1] / cum_speed.length), 2));
         $("#avg_calories").html(round((cum_calories[cum_calories.length - 2] / cum_calories.length), 2));
-        $("#total_calories").html(round((cum_calories[cum_calories.length - 2]), 2));
+        $("#total_calories").html(round((cum_calories[cum_calories.length - 2]), 2));	
+		$("#avg_watts").html(round(((cum_avg_watts[cum_avg_watts.length - 2]) / cum_avg_watts.length), 2));
     }
 });
